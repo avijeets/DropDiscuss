99bcc248c686cfde9365ebe44ca444bdb8a524f2
avijeet <a@avijeets.com>
2019-04-30 23:56:32 -0400

    firebase dataservice

---
+++ DropDiscuss/Services/DataService.swift
@@ -0,0 +1,38 @@
+//
+//  DataService.swift
+//  DropDiscuss
+//
+//  Created by Avijeet Sachdev on 4/30/19.
+//  Copyright © 2019 Avijeet Sachdev. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBUser(uid: String, userData: [String: Any]) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+}
