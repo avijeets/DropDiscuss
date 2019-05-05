486d5cd9ceaa6ecfdc463337b64501f1fec88cd9
avijeet <a@avijeets.com>
2019-05-04 23:10:06 -0400

    profile view built

---
+++ DropDiscuss/Controllers/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  DropDiscuss
+//
+//  Created by Avijeet Sachdev on 5/4/19.
+//  Copyright © 2019 Avijeet Sachdev. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileTableView: UITableView!
+    @IBOutlet weak var profileName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signOutPressed(_ sender: Any) {
+    }
+    
+
+}
