5b812b48fba1c2533ce04b7e1681c265f80a9068
avijeet <a@avijeets.com>
2019-05-12 00:21:57 -0400

    email fetching from Firebase

--- DropDiscuss/Controllers/ComposeGroupViewController.swift
+++ DropDiscuss/Controllers/ComposeGroupViewController.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import Firebase
 
 class ComposeGroupViewController: UIViewController {
     @IBOutlet weak var titleTextfield: InsetTextField!
     @IBOutlet weak var descriptionTextfield: InsetTextField!
     @IBOutlet weak var emailSearchTextfield: InsetTextField!
     
-    @IBOutlet weak var groupMemberStack: UIStackView!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var groupMemberLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    
+    var emailArray = [String]()
+    var chosenUserArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        self.tableView.backgroundColor = UIColor(hex: "#4F5462")
+        
+        emailSearchTextfield.delegate = self
+        emailSearchTextfield.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        doneButton.isHidden = true
+    }
+    
+    @objc
+    func textFieldDidChange(){
+        if emailSearchTextfield.text == ""{
+            emailArray =  []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTextfield.text!) { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            }
+        }
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
@@ -33,18 +61,45 @@ class ComposeGroupViewController: UIViewController {
 
 extension ComposeGroupViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return emailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as? UserTableViewCell else { return UITableViewCell() }
         
         let profileImage = UIImage.init(named: "defaultProfileImage")
-        
-        cell.configureCell(profileImage: profileImage!, email: "email@email.com", isSelected: true)
+        if chosenUserArray.contains(emailArray[indexPath.row]) {
+            cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
+        }
+        else {
+            cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: false)
+        }
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? UserTableViewCell else { return }
+        
+        if !chosenUserArray.contains(cell.emailLabel.text!) {
+            chosenUserArray.append(cell.emailLabel.text!)
+            groupMemberLabel.text = chosenUserArray.joined(separator: ", ")
+            doneButton.isHidden = false
+        }
+        else {
+            chosenUserArray = chosenUserArray.filter({ $0 != cell.emailLabel.text! })
+            
+            if chosenUserArray.count >= 1 {
+                groupMemberLabel.text = chosenUserArray.joined(separator: ", ")
+            }
+            else {
+                groupMemberLabel.text = "Add people to your group ..."
+                doneButton.isHidden = true
+            }
+        }
+    }
+    
     
 }
+
+extension ComposeGroupViewController: UITextFieldDelegate {}
